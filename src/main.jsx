78d6ac460f159a5eca60e8371401ddaca6a6e400
Keri <keribienert@KB-MacBook-Pro.local>
2026-04-01 10:32:54 -0700

    fix doble portfolio render

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import KeriBienertPortfolio from './KeriBienertPortfolio.jsx' 
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<<<<<<< HEAD
     <App />
-    <KeriBienertPortfolio /> 
-  </StrictMode>,
-)
-=======
-    <App />  {/* Only render App */}
   </StrictMode>
-)
->>>>>>> a7e4d96 (Updated portfolio  with latest changes)
+);
